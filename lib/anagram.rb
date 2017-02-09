8b89d00fad7a45b20a3d370b4bf179a3011b15d8
gee2789 <geeyung.li@gmail.com>
2017-02-08 20:50:48 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor(:word)
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |word|
+      if @word.chars.sort.join == word.chars.sort.join
+        word
+      end
+    end
+  end
+end
